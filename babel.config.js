8b3c6452bfa653d2676b61afd5b90e50cfa78c11
Seah Chin Ying <scy.6char@gmail.com>
2019-01-14 15:48:49 +0800

    Use Quasar Framework

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: [
+  'presets': [
     '@vue/app'
+  ],
+  'plugins': [
+    [
+      'transform-imports',
+      {
+        'quasar': {
+          'transform': 'quasar-framework/dist/babel-transforms/imports.mat.js',
+          'preventFullImport': true
+        }
+      }
+    ]
   ]
 }

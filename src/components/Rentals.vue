9715dc9876cfb1b1dde629ea117811aa29bfe78c
Seah Chin Ying <scy.6char@gmail.com>
2019-01-15 18:39:48 +0800

    Update rentals layout

--- src/components/Rentals.vue
+++ src/components/Rentals.vue
@@ -4,11 +4,20 @@
     <div>
       <p>You have loaned <strong>{{ rentals.length }}</strong> bags</p>
     </div>
-    <ul>
-      <li v-for="(rental, index) in rentals" :key="index">
-        {{ rental.location }} | Expiring {{ rental.date }}
-      </li>
-    </ul>
+
+    <div class="rental-bags-list">
+      <div v-for="(rental, index) in rentals" :key="index"
+        class="rental-bag-item">
+        <div class="bag-rental-item-left">
+          <p>Bag #{{ index }}</p>
+          <p>{{ rental.location }}</p>
+        </div>
+        <div class="bag-rental-item-right">
+          <p class="rental-status">{{ rental.status }}</p>
+          {{ rental.date }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,4 +45,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.rentals-container {
+  padding: 2em;
+}
+
+.rental-bags-list {
+  margin-top: 2em;
+}
+
+.rental-status {
+  font-weight: 700;
+  color: $trashyGreen;
+}
+
+.rental-bag-item {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
